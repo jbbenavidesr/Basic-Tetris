68a16d86d5d37cdb84b61ac6266723c389dc92c6
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2020-05-23 13:47:49 -0500

    Starting the files and main markup and style.

---
+++ app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid__square'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySellector('#start-button')
+    const width = 10;
+
+    
+
+})

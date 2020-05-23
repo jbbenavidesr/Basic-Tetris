1c21da5602609c678c72eafb9913eb9eb5ec5f8e
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2020-05-23 14:44:10 -0500

    Start with random tetramino

--- app.js
+++ app.js
@@ -5,50 +5,54 @@ document.addEventListener('DOMContentLoaded', () => {
     const StartBtn = document.querySelector('#start-button')
     const width = 10
 
-  // The Tetrominoes
-  const lTetromino = [
-      [1, width+1, width*2+1, 2],
-      [width, width+1, width+2, width*2+2],
-      [1, width+1, width*2+1, width*2],
-      [width, width*2, width*2+1, width*2+2]
-  ]
-  const zTetramino = [
-      [width+1, width+2, width*2, width*2+1],
-      [0, width, width+1, width*2+1],
-      [width+1, width+2, width*2, width*2+1],
-      [0, width, width+1, width*2+1]
-  ] 
-  const tTetramino = [
-    [1, width, width+1, width+2],
-    [1, width+1, width+2, width*2+1],
-    [width, width+1, width+2, width*2+1],
-    [1, width, width+1, width*2+1]
-  ]
-  const oTetramino = [
-      [0, 1, width, width+1],
-      [0, 1, width, width+1],
-      [0, 1, width, width+1],
-      [0, 1, width, width+1]
-  ]
-  const iTetramino = [
-      [1, width+1, width*2+1, width*3+1],
-      [width, width+1, width+2, width+3],
-      [1, width+1, width*2+1, width*3+1],
-      [width, width+1, width+2, width+3]
-  ]
+    // The Tetrominoes
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+    const zTetramino = [
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1]
+    ] 
+    const tTetramino = [
+        [1, width, width+1, width+2],
+        [1, width+1, width+2, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1]
+    ]
+    const oTetramino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ]
+    const iTetramino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+    ]
 
-  const theTetraminoes = [lTetromino, zTetramino, tTetramino, oTetramino, iTetramino]
+    const theTetraminoes = [lTetromino, zTetramino, tTetramino, oTetramino, iTetramino]
 
-  let currentPosition = 4
-  let current = theTetraminoes[0][0]
+    let currentPosition = 4
+    let currentRotation = 0
 
-  //draw the first rotation in the first tetramino
-  function draw() {
-      current.forEach(index => {
-          squares[currentPosition + index].classList.add('tetramino')
-      })
-  }
+    // Randomly select a Tetramino and its first rotation
+    let random = Math.floor(Math.random()*theTetraminoes.length)
+    let current = theTetraminoes[random][currentRotation]
 
-  draw()
+    //draw the first rotation in the first tetramino
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetramino')
+        })
+    }
+
+    draw()
 
 })
